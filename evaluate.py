c794cb71ecece1ca36e32e4e439df16b305d035a
Joshua Meisel <joshua.a.meisel@gmail.com>
2019-05-11 18:57:37 -0400

    update to main repo

--- evaluate.py
+++ evaluate.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys 
-from pyspark.mllib.evaluation import RegressionMetrics
+from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALSModel
+from timeit import default_timer as timer
      
 def already_trained(rank, reg, alpha):
     return True
 
 # Only enter this block if we're in main
 def main(spark, user):
-    fname = 'als_rank_%d_reg_%s_alpha_%s' % (10, .1, 1.0)
-    model = ALSModel.load('hdfs:/user/%s/%s' % (user, fname))
+    # fname = 'als_rank_%d_reg_%s_alpha_%s' % (10, .1, 1.0)
+    # model = ALSModel.load('hdfs:/user/%s/%s' % (user, fname))
+    model = ALSModel.load('hdfs:/user/%s/als_test' % user)
     val_df = spark.read.parquet('hdfs:/user/%s/val_full_indexed.parquet' % user)
     predictions = model.transform(val_df)
     evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",
@@ -21,7 +23,7 @@ def main(spark, user):
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
-    print('starting')
+    start = timer()
     # Create the spark session object
     spark = SparkSession.builder.appName('train').getOrCreate()
 
@@ -29,3 +31,5 @@ if __name__ == "__main__":
 
     # Call our main routine
     main(spark, user)
+    end = timer()
+    print(end - start)
--- train.py
+++ train.py
@@ -5,34 +5,32 @@ import random
 import itertools
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS
+from timeit import default_timer as timer
      
 def already_trained(rank, reg, alpha):
     return True
 
 # Only enter this block if we're in main
-def main(spark, user):
-    train_df = spark.read.parquet('hdfs:/user/jm7955/sub_train.parquet')
+def main(spark, user, alpha):
+    train_df = spark.read.parquet('hdfs:/user/jm7955/train_full_indexed.parquet')
     path = 'hdfs:/user/%s/' % user
 
-    ranks = (8, 10, 12)
-    regs = (.01, .1, 1)
-    alphas = (1.0, 2.0, 3.0, 4.0)
-    params_list = list(itertools.product(ranks, regs, alphas))
+    ranks = (10, 20, 30, 40)
+    regs = (.001, .01, .1, 1)
+    params_list = list(itertools.product(ranks, regs))
     random.shuffle(params_list)
 
-    rank, reg, alpha = (10, .1, 1.0)
-    als = ALS(regParam=reg, rank=rank, alpha=alpha, ratingCol="count")
-    model = als.fit(train_df)
-    #model.save(path + 'als_test')
-    model.save(path + 'als_rank_%d_reg_%s_alpha_%s' % (10, .1, 1.0))
-
     for params in params_list:
-        rank, reg, alpha = params
+        rank, reg = params
         if already_trained(rank, reg, alpha):
             continue
+
+        print('starting with alpha=%s, rank=%d, reg=%s' % (rank, reg, alpha))
+        start = timer()
         als = ALS(regParam=reg, rank=rank, alpha=alpha, ratingCol="count")
         model = als.fit(train_df)
         model.save(path + 'als_rank_%d_reg_%s_alpha_%s' % params)
+        print('finished in %s seconds' % timer() - start)
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
@@ -42,7 +40,7 @@ if __name__ == "__main__":
 
     user = sys.argv[1]
 
-
+    alpha = sys.argv[2]
 
     # Call our main routine
-    main(spark, user)
+    main(spark, user, float(alpha))
